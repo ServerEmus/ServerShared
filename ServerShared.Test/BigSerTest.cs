91f03df2ffedd7a4013d016dbd8b561907abb840
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-10-09 23:22:19 +0200

    more docs test for bigsers

---
+++ ServerShared.Test/BigSerTest.cs
@@ -0,0 +1,109 @@
+﻿using ServerShared.IO;
+
+namespace ServerShared.Test;
+
+public class BigSer : IBigSerializable
+{
+    public string StrData = string.Empty;
+    public int Other;
+    public long LongTest;
+    public DateTimeOffset Time;
+    public void Deserialize(BinaryReaderBig reader)
+    {
+        StrData = reader.ReadString();
+        Other = reader.ReadInt32();
+        LongTest = reader.ReadInt64();
+        Time = DateTimeOffset.FromFileTime(reader.ReadInt64());
+    }
+
+    public void Serialize(BinaryWriterBig writer)
+    {
+        writer.Write(StrData);
+        writer.Write(Other);
+        writer.Write(LongTest);
+        writer.Write(Time.ToFileTime());
+    }
+}
+
+public class BigSer2 : IBigInstanceSerializable<BigSer2>
+{
+    public string StrData = string.Empty;
+    public int Other;
+    public long LongTest;
+    public DateTimeOffset Time;
+    public void Deserialize(BinaryReaderBig reader)
+    {
+        StrData = reader.ReadString();
+        Other = reader.ReadInt32();
+        LongTest = reader.ReadInt64();
+        Time = DateTimeOffset.FromFileTime(reader.ReadInt64());
+    }
+
+    public void Serialize(BinaryWriterBig writer)
+    {
+        writer.Write(StrData);
+        writer.Write(Other);
+        writer.Write(LongTest);
+        writer.Write(Time.ToFileTime());
+    }
+    public static BigSer2? Parse(BinaryReaderBig reader)
+    {
+        return new()
+        { 
+            StrData = reader.ReadString(),
+            Other = reader.ReadInt32(),
+            LongTest = reader.ReadInt64(),
+            Time = DateTimeOffset.FromFileTime(reader.ReadInt64())
+        };
+    }
+}
+
+public class BigSerTest
+{
+    [Fact]
+    public void Test_BigSer()
+    {
+        BigSer ser = new()
+        {
+            StrData = "AAAAAAAAAAAA",
+            LongTest = 555,
+            Other = 6456,
+            Time = DateTimeOffset.UtcNow,
+        };
+
+        using MemoryStream writeStream = new();
+        using BinaryWriterBig writerBig = new(writeStream);
+        writerBig.WriteSerializable(ser);
+        using MemoryStream readerStream = new(writeStream.ToArray());
+        using BinaryReaderBig readerBig = new(readerStream);
+        BigSer deser = readerBig.ReadSerializable<BigSer>();
+        Assert.Equal(ser.StrData, deser.StrData);
+        Assert.Equal(ser.LongTest, deser.LongTest);
+        Assert.Equal(ser.Other, deser.Other);
+        Assert.Equal(ser.Time, deser.Time);
+    }
+
+    [Fact]
+    public void Test_BigSerInstance()
+    {
+        BigSer2 ser = new()
+        {
+            StrData = "AAAAAAAAAAAA",
+            LongTest = 555,
+            Other = 6456,
+            Time = DateTimeOffset.UtcNow,
+        };
+
+        using MemoryStream writeStream = new();
+        using BinaryWriterBig writerBig = new(writeStream);
+        writerBig.WriteSerializable(ser);
+        using MemoryStream readerStream = new(writeStream.ToArray());
+        using BinaryReaderBig readerBig = new(readerStream);
+        BigSer2? deser = readerBig.ReadInstanceSerializable<BigSer2>();
+        Assert.NotNull(deser);
+        Assert.Equal(ser.StrData, deser.StrData);
+        Assert.Equal(ser.LongTest, deser.LongTest);
+        Assert.Equal(ser.Other, deser.Other);
+        Assert.Equal(ser.Time, deser.Time);
+    }
+}
--- ServerShared/IO/IBigSerializable.cs
+++ ServerShared/IO/IBigSerializable.cs
@@ -1,25 +1,25 @@
 ﻿namespace ServerShared.IO;
 
 /// <summary>
-/// 
+/// Serializable interface for big stream writer/reader.
 /// </summary>
 public interface IBigSerializable
 {
     /// <summary>
-    /// 
+    /// Serialize <see langword="this"/> into <paramref name="writer"/>.
     /// </summary>
     /// <param name="writer">The stream to write data to.</param>
     public void Serialize(BinaryWriterBig writer);
 
     /// <summary>
-    /// 
+    /// Read <see langword="this"/> from <paramref name="reader"/>. 
     /// </summary>
     /// <param name="reader">The stream to read data to.</param>
     public void Deserialize(BinaryReaderBig reader);
 }
 
 /// <summary>
-/// 
+/// Serializable interface for getting <typeparamref name="T"/> with big stream writer/reader.
 /// </summary>
 /// <typeparam name="T">Any type to serialize.</typeparam>
 public interface IBigInstanceSerializable<T> : IBigSerializable
@@ -29,5 +29,5 @@ public interface IBigInstanceSerializable<T> : IBigSerializable
     /// </summary>
     /// <param name="reader">The <see cref="BinaryReaderBig"/> to parse.</param>
     /// <returns>The result of parsing <paramref name="reader"/>.</returns>
-    public abstract static T Parse(BinaryReaderBig reader);
+    public abstract static T? Parse(BinaryReaderBig reader);
 }
